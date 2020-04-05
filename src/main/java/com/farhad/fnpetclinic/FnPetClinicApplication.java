5bd7059123f92b3a52999f6fcae3d62ee9922d9e
Farhad Nasimi <farhad.nasimi@gmail.com>
2020-04-04 19:09:25 -0500

    Initial commit after project set up.

---
+++ src/main/java/com/farhad/fnpetclinic/FnPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.farhad.fnpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FnPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farhad/fnpetclinic/FnPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhad.fnpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
